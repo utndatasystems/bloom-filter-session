1e14a81dcd43d9a89a54725de7c6b42bd81855ec
Ping-Lin Kuo <ping-lin.kuo@utn.de>
2025-12-10 17:12:50 +0100

    Update the api url

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://bloom-filter.ddns.net:5000/';
 
 const valueInput = document.getElementById('valueInput');
 const insertBtn = document.getElementById('insertBtn');
